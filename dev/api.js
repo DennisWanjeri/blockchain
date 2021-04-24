f579ecae81a52f64f938856351443bb474fcc3d2
DennisWanjeri <shikandadennis07@gmail.com>
2021-04-24 19:34:36 +0300

    api testing

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,18 @@
-var express = require('express');
-var app = express()
-app.get('/', function (req, res) {
-res.send('Hello World')
-})
-app.listen(3000, function() {
-    console.log('listening on port 3000...');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+;
+app.get('/blockchain', function (req, res) {
+});
+app.post('/transaction', function (req, res) {
+console.log(req.body);
+    res.send('The amount of the transaction is {req.body.amount} bitcoin');
+});
+app.get('/mine', function(req, res) {
+});
+app.listen(3000, function(){
+    console.log('listening on port 3000 ..');
 });
--- dev/networkNode.js
+++ dev/networkNode.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.get('/blockchain', function (req, res) {
 });
-
-app.post('/transaction' function(req, res) {
+app.post('/transaction', function (req, res) {
+console.log(req.body);
+    res.send('The amount of the transaction is ${req.body.amount} bitcoin');
 });
 app.get('/mine', function(req, res) {
 });
 app.listen(3000, function(){
-    consope.log('listening on port 3000...');
+    console.log('listening on port 3000 ..');
 });
