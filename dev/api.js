01d6ca7f4a3cf1196f43e1f725e5a686eb1573a4
DennisWanjeri <shikandadennis07@gmail.com>
2021-04-24 17:12:47 +0300

    postman and api setup

--- dev/api.js
+++ dev/api.js
@@ -3,4 +3,6 @@ var app = express()
 app.get('/', function (req, res) {
 res.send('Hello World')
 })
-app.listen(3000)
+app.listen(3000, function() {
+    console.log('listening on port 3000...');
+});
---
+++ dev/networkNode.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/blockchain', function (req, res) {
+});
+
+app.post('/transaction' function(req, res) {
+});
+app.get('/mine', function(req, res) {
+});
+app.listen(3000, function(){
+    consope.log('listening on port 3000...');
+});
